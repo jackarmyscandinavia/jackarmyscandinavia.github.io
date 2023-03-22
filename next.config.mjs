54b18143feee0eda94e5affb856ca7def4729da5
Eirik Reksten <eireksten@gmail.com>
2023-03-22 10:45:17 +0100

    Fewer optimized variants of images

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
-/**
- * @type {import('next').NextConfig}
- */
+
 
 import withExportImages from 'next-export-optimize-images';
 
-const nextConfig = withExportImages({});
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = withExportImages({
+  images: {
+    deviceSizes: [640, 960, 1080, 1920],
+    imageSizes: [64, 128, 256, 384],
+  }
+});
 
 export default nextConfig;
